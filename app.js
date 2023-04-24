be9d08fec7c34682c0f3f12d4322c4e8cf461e1a
Abhishek777777777 <abhishekmishra992016@gmail.com>
2023-04-24 14:01:23 +0530

    classAding

--- app.js
+++ app.js
@@ -12,11 +12,49 @@
 // console.log(document.forms);
 // console.log(document.links);
 
+//1.Id selector
 const main = document.getElementById('main-header');
 main.style.borderBottom = 'solid 4px black';
 
 const title = document.querySelector('.title');
 title.style.fontWeight = 'bold';
 title.style.color = 'green';
+
+
+//2.Class selector
+
+const items = document.getElementsByClassName('list-group-item');
+console.log(items);
+console.log(items[1]);
+// items[0].textContent = 'This is first list';
+// items[1].textContent = 'This is second list';
+// items[2].textContent = 'This is third list';
+// items[3].textContent = 'This is fourth list';
+// items[0].style.fontWeight = 'bold';
+// items[1].style.fontWeight = 'bold';
+// items[2].style.fontWeight = 'bold';
+// items[3].style.fontWeight = 'bold';
+
+ items[2].style.backgroundColor = 'green';
+
+for(let i=0;i<items.length;i++){
+    items[i].style.fontWeight = 'bold';
+}
+
+
+// items[0].style.backgroundColor = 'Violet';
+// items[1].style.backgroundColor = 'LightGray';
+// items[2].style.backgroundColor = 'Orange'
+// items[3].style.backgroundColor = 'Tomato';
+
+//    for(let i=0;i<items.length;i++){
+//     items[i].style.backgroundColor = 'LightGray'; 
+//    }
+
+// items[0].style.color = 'Tomato';
+// items[1].style.fontWeight = 'DodgerBlue';
+// items[2].style.fontWeight = 'MediumSeaGreen';
+// items[3].style.fontWeight = 'LightGray';
+
 
 
