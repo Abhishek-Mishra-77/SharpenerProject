a4a155ac94bfa132c5cfb580a00cdb6a686d6dcf
Abhishek777777777 <abhishekmishra992016@gmail.com>
2023-05-07 22:00:50 +0530

    myNew

--- chech.js
+++ chech.js
@@ -1,13 +1,13 @@
-const obj= {
+const obj = {
 
   "key1": "value1",
-  
-  "key2" : "value2",
-  
-  "key3" : "value3"
-  
-  }
-  
-  const obj2 = { ...obj}
-  
-  console.log(obj2 === obj)
+
+  "key2": "value2",
+
+  "key3": "value3"
+
+}
+
+const obj2 = { ...obj }
+
+console.log(obj2 === obj)
