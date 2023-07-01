2542eec82c2a138a08324d572239917cd477975d
Abhishek777777777 <abhishekmishra992016@gmail.com>
2023-07-01 21:19:50 +0530

    ExpenseTracker Done

--- main JavaScript Project/ExpenseTrackerApp.js
+++ main JavaScript Project/ExpenseTrackerApp.js
@@ -10,17 +10,7 @@ btn.addEventListener('click', ExpenseTracker);
 
 function ExpenseTracker(event) {
     event.preventDefault();
-    const deleteBtn = document.createElement('input');
-    deleteBtn.type = 'button';
-    deleteBtn.value = 'Delete Expenses';
 
-    // Edit button
-
-    const editBtn = document.createElement('input');
-    editBtn.type = 'button';
-    editBtn.value = 'Edit Expenses'
-
-    //Add All details
     const ExpenseAmount = Expense.value;
     const DescriptionName = Description.value;
     const selectthing = select.value;
@@ -32,9 +22,15 @@ function ExpenseTracker(event) {
     };
 
     localStorage.setItem(details.DescriptionName, JSON.stringify(details));
+    addItems(details)
+}
 
 
-    if (ExpenseAmount.value === 0 || DescriptionName.value === '' || selectthing.value === '') {
+
+// AddItems here we are Take input and add to Html 
+const addItems = (obj) => {
+
+    if (obj.ExpenseAmount.value === 0 || obj.DescriptionName.value === '' || obj.selectthing.value === '') {
         msg.style.background = 'red';
         msg.innerHTML = 'Please fill the form !';
         setTimeout(() => msg.remove(), 5000)
@@ -42,35 +38,52 @@ function ExpenseTracker(event) {
     else {
         const li = document.createElement('li');
 
-        li.appendChild(document.createTextNode(`${Expense.value} - ${Description.value} - ${select.value} - `));
-        li.appendChild(deleteBtn);
-        li.appendChild(editBtn);
+        li.appendChild(document.createTextNode(`${obj.ExpenseAmount}Rs - ${obj.DescriptionName} - ${obj.selectthing} - `));
+        li.appendChild(deleteButton(li, obj));
+        li.appendChild(editButton(li, obj));
         userList.appendChild(li);
 
         Expense.value = null;
         Description.value = ' ';
         select.value = ' ';
-
-        // Delete section
-
-        deleteBtn.onclick = () => {
-            localStorage.removeItem(details.DescriptionName);
-            userList.removeChild(li);
-        }
-
-        // Edit section
-
-        editBtn.onclick = () => {
-            Expense.value = details.ExpenseAmount;
-            Description.value = details.DescriptionName;
-            select.value = details.selectthing;
-            localStorage.removeItem(details.DescriptionName);
-            userList.removeChild(li);
-        }
-
     }
-
 }
+
+
+
+// Delete button for deleting Items for browser also localeStrorage
+
+const deleteButton = (li, obj) => {
+    const deleteBtn = document.createElement('input');
+    deleteBtn.type = 'button';
+    deleteBtn.value = 'Delete';
+    deleteBtn.onclick = () => {
+        localStorage.removeItem(obj.DescriptionName);
+        userList.removeChild(li);
+    }
+    return deleteBtn
+}
+
+
+
+
+// Edit button for editing items for browser also localestorage
+
+const editButton = (li, obj) => {
+    console.log(obj)
+    const editBtn = document.createElement('input');
+    editBtn.type = 'button';
+    editBtn.value = 'Delete';
+    editBtn.onclick = () => {
+        const data = localStorage.removeItem(obj.email);
+        userList.removeChild(li);
+        Expense.value = obj.DescriptionName;
+        Description.value = obj.ExpenseAmount;
+        select.value = obj.selectthing;
+    }
+    return editBtn
+}
+
 
 
 // When the page refres all data will showing in the web page
@@ -80,35 +93,12 @@ window.addEventListener('load', (event) => {
     const data = Object.keys(localStorage);
 
     data.forEach((key) => {
-        const li = document.createElement('li');
         const details = JSON.parse(localStorage.getItem(key));
-
-        li.appendChild(document.createTextNode(`${details.ExpenseAmount} - ${details.DescriptionName} - ${details.selectthing} `));
-        const deleteBtn = document.createElement('input');
-        deleteBtn.type = 'button';
-        deleteBtn.value = 'Delete Expense';
-        li.appendChild(deleteBtn);
-
-        const editBtn = document.createElement('input');
-        editBtn.type = 'button';
-        editBtn.value = 'Edit Expense';
-        li.appendChild(editBtn);
-
-        deleteBtn.onclick = () => {
-            localStorage.removeItem(details.DescriptionName);
-            userList.removeChild(li);
-        }
-
-        editBtn.onclick = () => {
-            Expense.value = details.ExpenseAmount;
-            Description.value = details.DescriptionName;
-            select.value = details.selectthing;
-            localStorage.removeItem(details.DescriptionName);
-            userList.removeChild(li);
-        }
-
-        userList.appendChild(li);
-
-    });
+        addItems(details);
+    })
 
 });
+
+
+
+
